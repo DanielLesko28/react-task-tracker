bd380cf4d909b9999377b37202036ed6514a9199
Daniel Lesko <daniel.lesko@publicisgroupe.cz>
2023-11-16 18:05:24 +0100

    working on task form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-
+import "./App.css"
+import TaskForm from "./components/taskForm/TaskForm";
 
 function App() {
   return (
-   <div>
-    <h1>Hello Daniel</h1>
+   <div className="app">
+    <header className="app_header">header</header>
+    <TaskForm />
+    <main className="app_main">
+      <section className="task_section">section 1</section>
+      <section className="task_section">section 2</section>
+      <section className="task_section">section 3</section>
+    </main>
    </div>
   );
 }
